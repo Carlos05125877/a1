5ffd5950a09bb96d9deecd7ecd5a7af5d0a439b8
CarlosOSuza05 <carloseduardosouzacarvalho01@gmail.com>
2024-10-30 21:58:20 -0300

    Update

--- src/Assento.java
+++ src/Assento.java
@@ -1,6 +1,21 @@
-public class Assento {
+import javax.swing.*;
+import java.awt.*;
+
+public class Assento extends JFrame {
     private int id;
     private Voo voo;
     private boolean ocupado;
     // ... outros atributos e métodos
+
+    public Assento() {
+        setTitle("Nova Página");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        JLabel label = new JLabel("Bem-vindo à Nova Página!");
+        label.setFont(new Font("Arial", Font.BOLD, 20));
+        add(label);
+
+    }
 }
--- src/Reserva.java
+++ src/Reserva.java
@@ -1,10 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.Date;
 
-public class Reserva {
+public  class Reserva extends JFrame  {
     private int id;
     private Usuario usuario;
     private Voo voo;
     private Date dataReserva;
     private String status;
+
+    public Reserva() {
+        setTitle("Reservas");
+        setSize(800, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setLayout(new BorderLayout());
+
+        JLabel label = new JLabel("Realizar reserva");
+        label.setFont(new Font("Arial", Font.BOLD, 20));
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        add(label, BorderLayout.NORTH);
+
+
+    }
+
     // ... outros atributos e métodos
+
 }
